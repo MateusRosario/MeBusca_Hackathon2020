8ba7dc223abfae90f764f2aac8ac54e17b7af6b9
MateusRosario <mateusdasilvarusario@gmailcom>
2020-11-24 16:26:10 -0300

    refactor: Basic Files refactored to begin implementations
    Main and Home Widgets separated

---
+++ hackathon_code_is_easy19/lib/homepage/consumer.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ConsumerHomePage extends StatefulWidget {
+  final String title;
+
+  ConsumerHomePage({Key key, this.title}) : super(key: key);
+
+  @override
+  _ConsumerHomePageState createState() => _ConsumerHomePageState();
+}
+
+class _ConsumerHomePageState extends State<ConsumerHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      bottomNavigationBar: Container(
+        child: Row(
+          children: <Widget>[],
+        ),
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10))),
+      ),
+    );
+  }
+}
