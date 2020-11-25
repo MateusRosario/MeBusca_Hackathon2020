c0fe0c41b89f37f5151b7edf63cf7234cedf4801
MateusRosario <mateusdasilvarusario@gmailcom>
2020-11-25 03:01:31 -0300

    feature: Main Screen Layout Implemented

--- hackathon_code_is_easy19/lib/Products.dart
+++ hackathon_code_is_easy19/lib/Products.dart
@@ -1,4 +1,3 @@
-import 'package:MeBusca/root.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:MeBusca/kart.dart';
--- hackathon_code_is_easy19/lib/kart.dart
+++ hackathon_code_is_easy19/lib/kart.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:MeBusca/Products.dart';
 
 class KartPage extends StatefulWidget {
   KartPage({
